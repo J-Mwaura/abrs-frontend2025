2fb1bc5790fd0e2a930ae21cfbdd3f3d547668f3
J-Mwaura <atjkm@yahoo.com>
2026-01-10 23:37:01 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'abrs-frontend2025',
+  webDir: 'www'
+};
+
+export default config;
