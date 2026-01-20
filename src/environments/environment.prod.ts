fc356ae2f47f27eebd5d70c3ad4afeb295aa5177
J-Mwaura <atjkm@yahoo.com>
2026-01-20 18:28:09 +0000

    Signed-off-by: J-Mwaura <atjkm@yahoo.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/',
-  //apiUrl: 'https://revenue-backend-bf7e5e6a786a.herokuapp.com/'
+  apiUrl: 'https://revenue-backend-bf7e5e6a786a.herokuapp.com/'
 };
